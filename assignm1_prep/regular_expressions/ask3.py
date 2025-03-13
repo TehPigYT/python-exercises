c34f3f13181b6ca21c488c2d29ac36ad16b2e041
TehPig <aggelosdellas@gmail.com>
2025-03-13 02:45:45 +0200

    fix

--- assignm1_prep/regular_expressions/ask3.py
+++ assignm1_prep/regular_expressions/ask3.py
@@ -1,6 +1,6 @@
 import re
 
 txt = "this is a text that includes youtube@gmail.com email address wow even amazon@twitch.tv exists and even discord@service.com"
-//pattern = r"\[a-z][A-Z]+\@\c+.\c+"
+#pattern = r"\[a-z][A-Z]+\@\c+.\c+"
 matches = re.findall(pattern, txt)
 print(matches)
