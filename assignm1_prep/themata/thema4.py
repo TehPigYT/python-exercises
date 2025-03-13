96ca4370429c75a2192327dc4528e9d54a9397f4
TehPig <aggelosdellas@gmail.com>
2025-03-13 02:58:04 +0200

    Add missing changes

--- assignm1_prep/themata/thema4.py
+++ assignm1_prep/themata/thema4.py
@@ -1,5 +1,14 @@
-with open("sample_in.txt", "r") as file:
-    contents = file.read().strip()
+def read_file():
+    with open("sample_in.txt", "r") as file:
+        contents = file.read().strip()
 
-    filtered = [i.upper() for i in contents.split() if len(i) > 5]
-    print(filtered)
+        filtered = [i.upper() for i in contents.split() if len(i) > 5]
+        return filtered
+
+
+def save_file(words):
+    with open("sample_out.txt", "w") as file:
+        file.write("\n".join(words))
+
+words = read_file()
+save_file(words)
