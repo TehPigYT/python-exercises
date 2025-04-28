a0c581b1f89c9fd67dec09147e4af7459af1c34c
TehPig <aggelosdellas@gmail.com>
2025-04-29 00:42:41 +0300

    Add more lab4 assignments, finals tomorrow

---
+++ lab4/ask3.py
@@ -0,0 +1,48 @@
+import unittest
+
+
+class Stack:
+    list_empty = "Η στοίβα δεν περιέχει στοιχεία"
+
+    def __init__(self):
+        self.data = []
+
+    def push(self, item):
+        self.data.append(item)
+        # print(self.data)
+
+    def pop(self):
+        if len(self.data) < 1:
+            print(list_empty)
+            return
+
+        self.data.pop()
+        # print(self.data)
+
+    def clear(self):
+        self.data = []
+        # print("cleared")
+
+    def __str__(self):
+        if len(self.data) == 0:
+            return Stack.list_empty
+
+        str = "->" + "  ".join(f"{item}\n" for item in reversed(self.data))
+        # print(str)
+        return str
+
+class TestHammingDistance(unittest.TestCase):
+    def test_Stack(self):
+        s = Stack()
+        self.assertTrue(str(s)=="Η στοίβα δεν περιέχει στοιχεία")
+        s.push(1)
+        self.assertTrue(str(s)=="->1\n")
+        s.push(2)
+        self.assertTrue(str(s)=="->2\n  1\n")
+        s.push(3)
+        self.assertTrue(str(s)=="->3\n  2\n  1\n")
+        s.pop()
+        self.assertTrue(str(s)=="->2\n  1\n")
+
+if __name__ == "__main__":
+    unittest.main()
